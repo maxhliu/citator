b27bc154ea4f53a3940f8c482f0574973231ad8d
Max Liu <lium001@gmail.com>
2016-01-23 17:37:52 -0500

    Fixed issue where acts and lines would accumulate.

--- Citator_FX/src/application/Parse.java
+++ Citator_FX/src/application/Parse.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parse {
+    //returns true if one of b equals a
     public static boolean stringEquals(String a, String... b) {
         for (String s : b) {
             if (a.equals(s)) {
@@ -23,6 +24,7 @@ public class Parse {
         return false;
     }
 
+    //find child nodes with one of the given names
     public static List<Node> findChildNodesByName(Node root, String... names) {
         List<Node> returnNodes = new ArrayList<>();
         NodeList n = root.getChildNodes();
@@ -40,37 +42,52 @@ public class Parse {
     }
 
     public static List<Act> parseXML(String playName) throws ParserConfigurationException, IOException, SAXException {
+        //parse the xml into a Document object
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setIgnoringElementContentWhitespace(true);
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.parse(new File("xmlFiles/" + playName + ".xml"));
         Node root = doc.getDocumentElement();
 
+        //navigate through the act nodes, turning them into Act objects
         List<Act> acts = new ArrayList<>();
-        List<Scene> scenes = new ArrayList<>();
-        List<Line> lines = new ArrayList<>();
-
         List<Node> nodeActs = findChildNodesByName(root, "ACT");
-        int lineNumber = 0;
-        for (Node n1 : nodeActs) {
-            List<Node> nodeScenes = findChildNodesByName(n1, "SCENE");
-            for (Node n2 : nodeScenes) {
-                List<Node> nodeLines = findChildNodesByName(n2, "SPEECH", "STAGEDIR");
 
-                for (Node n3 : nodeLines) {
-                    if (n3.getNodeName().equals("SPEECH")) {
-                        lines.add(new Speech(n3.getFirstChild().getTextContent(),
-                                n3.getLastChild().getTextContent(), lineNumber++));
+        for (Node act : nodeActs) {
+            //start the line number, which resets after each act
+            int lineNumber = 0;
+            //navigate through the scene nodes, turning them into Scene objects
+            List<Node> nodeScenes = findChildNodesByName(act, "SCENE");
+            List<Scene> scenes = new ArrayList<>();
+
+            for (Node scene : nodeScenes) {
+                //navigate through the speech and stagedir nodes, turning them into Line objects
+                List<Line> lines = new ArrayList<>();
+                List<Node> nodeLines = findChildNodesByName(scene, "SPEECH", "STAGEDIR");
+
+                for (Node line : nodeLines) {
+                    //see if it's a speech or stagedir node
+                    if (line.getNodeName().equals("SPEECH")) {
+                        //speeches can start with a stagedir which doesn't count as a line
+                        //if it's a speech node it can have multiple lines inside
+                        lines.add(new Speech(line.getFirstChild().getTextContent(),
+                                line.getLastChild().getTextContent(), lineNumber++));
                     } else {
-                        lines.add(new StageDirection(n3.getFirstChild().getTextContent(), lineNumber++));
+                        lines.add(new StageDirection(line.getFirstChild().getTextContent(), lineNumber++));
                     }
-                }
-                scenes.add(new Scene(n2.getFirstChild().getTextContent(), lines));
 
+                }
+                //using the Line objects gathered from the inside for loop, create a Scene object
+                scenes.add(new Scene(scene.getFirstChild().getTextContent(), lines, act.getFirstChild().getTextContent()));
             }
-            acts.add(new Act(n1.getFirstChild().getTextContent(), scenes));
-            lineNumber = 0;
+            //using the Scene objects gathered from inside the for loops, create an Act object
+            acts.add(new Act(act.getFirstChild().getTextContent(), scenes));
         }
         return acts;
     }
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        parseXML();
+    }
+
 }
