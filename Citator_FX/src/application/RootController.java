891524e7c85d1d1df3ea329591289a1ad8faf1a4
Matthew Riley <mgriley97@gmail.com>
2016-01-23 17:38:32 -0500

    Merge branches 'master' and 'parse' of https://github.com/matresscow/citator into ui
    # Conflicts:
    #	Citator_FX/.idea/workspace.xml
    #	Citator_FX/bin/application/Act.class
    #	Citator_FX/bin/application/Main.class
    #	Citator_FX/bin/application/Scene.class
    
    Made Treeview of text acts and scenes

--- Citator_FX/src/application/RootController.java
+++ Citator_FX/src/application/RootController.java
@@ -1,8 +1,16 @@
 package application;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.*;
 import javafx.scene.layout.VBox;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,47 +19,141 @@ import java.util.List;
  */
 public class RootController {
 
+    private List<Scene> sceneList;
+    private int sceneIndex;
+
     @FXML
-    private VBox vBox;
+    private TextArea textArea;
+
+    @FXML
+    private TreeView treeView;
 
     @FXML
     protected void initialize() {
-        System.out.println("Initialize called");
-        configureText(vBox, getTestData());
-    }
 
-    private List<Act> getTestData() {
+        // Parse the play into a list of acts
+        List<Act> acts = null;
+        try {
+            acts = Parse.parseXML();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        List<Act> acts = new ArrayList<>();
-
-        Line[] lines = new Line[3];
-        lines[0] = new StageDirection("Enter Maximum Lius", 1);
-        lines[1] = new Speech("Max", "Wassup Brittany", 2);
-        lines[2] = new Speech("Andrew", "dsajflkjaskl;jdkfl;jak;sjl;fjsal;j;l", 3);
-
-        Scene[] scenes = new Scene[3];
-        scenes[0] = new Scene("First Scene", lines);
-        scenes[0].setActNumber("1");
-        scenes[1] = new Scene("Second Scene", lines);
-        scenes[1].setActNumber("1");
-        scenes[2] = new Scene("Third Scene", lines);
-        scenes[2].setActNumber("2");
-
-        acts.add(new Act("First Act", scenes));
-        acts.add(new Act("Second Act", scenes));
-        acts.add(new Act("Third Act", scenes));
-
-        return acts;
-    }
-
-    private void configureText(VBox vBox, List<Act> acts) {
+        // Init the treeView
+        TreeItem<String> root = new TreeItem<>("Hamlet"); // TODO: add play name
         for(Act a : acts) {
+            TreeItem<String> actItem = new TreeItem<>("Act " + a.getActNumber());
+            root.getChildren().add(actItem);
             for(Scene s : a.getScenes()) {
-                SceneArea sceneArea = new SceneArea(s);
-                sceneArea.actNum = a.getActNumber();
-                sceneArea.sceneNum = s.getSceneNumber();
-                vBox.getChildren().add(sceneArea);
+                TreeItem<String> sceneItem = new TreeItem<>("Scene " + s.getSceneNumber());
+                actItem.getChildren().add(sceneItem);
             }
         }
+        treeView.setRoot(root);
+        treeView.setEditable(false);
+
+        //
+        sceneList = generateSceneList(acts, sceneList);
+
+        // Start the textview at the first scene
+        sceneIndex = 0;
+        changeScene(sceneIndex);
     }
+
+    private List<Scene> generateSceneList(List<Act> acts, List<Scene> sceneList) {
+        List<Scene> scenes = new ArrayList<>();
+        for(Act a : acts) {
+            scenes.addAll(a.getScenes());
+        }
+        return scenes;
+    }
+
+    private void nextScene() {
+        if(sceneIndex < sceneList.size() - 1) {
+            sceneIndex++;
+            changeScene(sceneIndex);
+        }
+    }
+
+    private void lastScene() {
+        if(sceneIndex > 1) {
+            sceneIndex--;
+            changeScene(sceneIndex);
+        }
+    }
+
+    private void changeScene(int newIndex) {
+
+        Scene nextScene = sceneList.get(newIndex);
+
+        // Set the txt to the scene text
+        String txt = "Act " + nextScene.getActNumber() + ", " + nextScene.getTitle() + "\n\n";
+        System.out.flush();
+        for (Line l : nextScene.getLines()) {
+            if (l.isSpeech()) {
+                // TODO: use String.format to keep the line number right-justfied
+                Speech speech = (Speech) l;
+                txt += speech.getSpeaker();
+                txt += "\n" + speech.getText();
+                txt += "  " + speech.getLineNumber();
+            } else {
+                StageDirection stageDirection = (StageDirection) l;
+                txt += stageDirection.getText() + "   " + stageDirection.getLineNumber();
+            }
+            txt += "\n";
+        }
+        textArea.setText(txt);
+    }
+
+    public void onKeyReleased(KeyEvent evt) {
+
+        KeyCode kc = evt.getCode();
+
+        if(kc == KeyCode.UP) {
+            lastScene();
+        }
+        if(kc == KeyCode.DOWN) {
+            nextScene();
+        }
+        if (evt.isShortcutDown() && kc == KeyCode.C) {
+
+            // Get the current clipboard content
+            Clipboard cb = Clipboard.getSystemClipboard();
+            String selected = cb.getContent(DataFormat.PLAIN_TEXT)
+                    .toString();
+
+            // Replace the current clipboard content with the edited content
+            Scene selectedScene = sceneList.get(sceneIndex);
+            String edited = "\"" + selected + "\" (" + selectedScene.getActNumber() + "."
+                    + selectedScene.getSceneNumber() + ".TODO - TODO)"; // TODO
+            ClipboardContent content = new ClipboardContent();
+            content.putString(edited);
+            content.putHtml("bold: <b>" + edited + "</b>");
+            cb.setContent(content);
+        }
+    }
+
+//    private List<Act> getTestData() {
+//
+//        List<Act> acts = new ArrayList<>();
+//
+//        List<Line> lines = new ArrayList<>();
+//        lines.add(new StageDirection("Enter Maximum Lius", 1));
+//        lines.add(new Speech("Max", "Wassup Brittany", 2));
+//        lines[2] = new Speech("Andrew", "dsajflkjaskl;jdkfl;jak;sjl;fjsal;j;l", 3);
+//
+//        Scene[] scenes = new Scene[3];
+//        scenes[0] = new Scene("First Scene", lines);
+//        scenes[0].setActNumber("1");
+//        scenes[1] = new Scene("Second Scene", lines);
+//        scenes[1].setActNumber("1");
+//        scenes[2] = new Scene("Third Scene", lines);
+//        scenes[2].setActNumber("2");
+//
+//        acts.add(new Act("First Act", scenes));
+//        acts.add(new Act("Second Act", scenes));
+//        acts.add(new Act("Third Act", scenes));
+//
+//        return acts;
+//    }
 }
--- Citator_FX/src/application/SceneArea.java
+++ Citator_FX/src/application/SceneArea.java
@@ -15,43 +15,11 @@ public class SceneArea extends TextArea {
 
     public SceneArea(Scene scene) {
         super();
-        setEditable(false);
-
-        // Set the txt to the scene text
-        String txt = "Act " + scene.getActNumber() + ", " + scene.getTitle() + "\n\n";
-        System.out.flush();
-        for (Line l : scene.getLines()) {
-            if (l.isSpeech()) {
-                // TODO: use String.format to keep the line number right-justfied
-                Speech speech = (Speech) l;
-                txt += speech.getSpeaker();
-                txt += "\n" + speech.getText();
-                txt += "  " + speech.getLineNumber();
-            } else {
-                StageDirection stageDirection = (StageDirection) l;
-                txt += stageDirection.getText() + "   " + stageDirection.getLineNumber();
-            }
-            txt += "\n";
-        }
-        setText(txt);
 
         // Set the copying ability
         // TODO: use Andrew's code later
         setOnKeyReleased((evt) -> {
-            if (evt.isShortcutDown() && evt.getCode() == KeyCode.C) {
 
-                // Get the current clipboard content
-                Clipboard cb = Clipboard.getSystemClipboard();
-                String selected = cb.getContent(DataFormat.PLAIN_TEXT)
-                        .toString();
-
-                // Replace the current clipboard content with the edited content
-                String edited = "\"" + selected + "\" (" + actNum + "." + sceneNum + ".TODO - TODO)"; // TODO
-                ClipboardContent content = new ClipboardContent();
-                content.putString(edited);
-                content.putHtml("bold: <b>" + edited + "</b>");
-                cb.setContent(content);
-            }
         });
 
         // Size to the contents
